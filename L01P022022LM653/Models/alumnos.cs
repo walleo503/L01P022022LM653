ed504e53d96e449fdb5c622888f0f60596f0566a
walleo503 <walter.linares1@catolica.edu.sv>
2025-03-25 17:55:37 -0600

    f

--- L01P022022LM653/Models/alumnos.cs
+++ L01P022022LM653/Models/alumnos.cs
@@ -6,9 +6,9 @@ namespace L01P022022LM653.Models
     {
         [Key]
         public int id { get; set; }
-        public string? codigo { get; set; }
-        public string? nombre { get; set; } 
-        public string? apellidos { get; set; }
+        public string codigo { get; set; }
+        public string nombre { get; set; } 
+        public string apellidos { get; set; }
         public int dui { get; set; }
         public int estado { get; set; }
     }
