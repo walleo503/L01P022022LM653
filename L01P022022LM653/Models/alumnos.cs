da04ffa54ed9e6107ac9aab00aa833f75e38ae73
walleo503 <walter.linares1@catolica.edu.sv>
2025-03-25 17:23:13 -0600

    actual

---
+++ L01P022022LM653/Models/alumnos.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01P022022LM653.Models
+{
+    public class alumnos
+    {
+        [Key]
+        public int id { get; set; }
+        public string? codigo { get; set; }
+        public string? nombre { get; set; } 
+        public string? apellidos { get; set; }
+        public int dui { get; set; }
+        public int estado { get; set; }
+    }
+}
---
+++ L01P022022LM653/Models/departamentos.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L01P022022LM653.Models
+{
+    public class departamentos
+    {
+        [Key]
+        public int id { get; set; }
+        public string? departamento { get; set; }
+
+    }
+}
